baed0135cb81d288e436f3a9d52e004676a58c35
sejongdeveloper <sejonghumble@naver.com>
2020-07-19 23:24:00 +0900

    스프릥 mvc

---
+++ sampleservletpre/src/main/java/me/whiteship/practice/SampleservletpreApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleservletpreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleservletpreApplication.class, args);
+	}
+
+}
