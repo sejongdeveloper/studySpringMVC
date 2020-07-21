853cbe21f6c9fe36f5e61a91a4d4fee4bd136ec7
sejongdeveloper <sejonghumble@naver.com>
2020-07-21 23:44:59 +0900

    서블릿 필터

---
+++ servletAndFilter/javaservletdemo/src/main/java/me/whiteship/AppConfig.java
@@ -0,0 +1,9 @@
+package me.whiteship;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+}
---
+++ servletAndFilter/javaservletdemo/src/main/java/me/whiteship/HelloService.java
@@ -0,0 +1,10 @@
+package me.whiteship;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String getName() {
+        return "whiteship";
+    }
+}
